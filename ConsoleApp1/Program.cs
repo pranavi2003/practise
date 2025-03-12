f80abce65ba39f08d510fd2d8715602efebc5cef
pranavi <pranavireddy0987@gmail.com>
2025-03-12 18:51:57 -0400

    Added new feature

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOPInheritance
+{
+    // Parent Class: Vehicle
+    public class Vehicle
+    {
+        // Properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        // Constructor
+        public Vehicle(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+
+        // Method to display vehicle info
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Vehicle: {Make} {Model} ({Year}), Color: {Color}");
+        }
+    }
+}
